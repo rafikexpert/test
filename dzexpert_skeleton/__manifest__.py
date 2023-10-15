748bc39f6180a3c2d5c6931e084615b486489a93
root <imed@imed.ws>
2023-10-15 21:32:10 +0100

    skeleton module

---
+++ dzexpert_skeleton/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "DZExpert.com Skeleton",
+    'summary': "DZExpert.com Skeleton",
+    'description': """
+		DZExpert.com Skeleton
+    """, 
+    'price':600,
+    'currency':'EUR' ,
+    'author': "DZExpert.com",
+    'website': "http://www.dzexpert.com",
+    'category': 'Project', 
+    'version': '11.0.1.3',   
+    'depends': [
+		'stock',
+		'account',
+	],
+    'data': [
+		'security/security.xml',
+		'views/consolidation.xml',
+		'views/menu.xml', 
+		'data/cron.xml', 
+		'security/ir.model.access.csv',   
+    ],
+    'installable': True,
+    'application': True,   
+    'auto_install': False,
+}
---
+++ dzexpert_skeleton/models/skeleton.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from odoo import SUPERUSER_ID
+from num2words import num2words
+from odoo import api, models,fields
+from odoo.exceptions import UserError 
+from odoo import exceptions
+from datetime import datetime
+import math
+import random,string
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT, float_compare
+import logging
+_logger = logging.getLogger(__name__)
+
+
+
+	
+	
+class Consolidation(models.Model):
+	_name = 'dzexpert.skeleton'
+	_inherit=['mail.thread']
+	_description = "Skeleton"	
+	_order = 'name asc'
+
+	
+	active = fields.Boolean('Active', default=True)
+	
+	name = fields.Char(string='Nom', required=True,track_visibility='onchange')	
+	image_small = fields.Binary("Image", attachment=True)	
+	description=fields.Html(string='Description')
+	company_id = fields.Many2one('res.company', 'Entreprise Locale')
+		
